826ef1ab81995fd531513d73e10e14bba71ef874
cvassalos <vassalce@gmail.com>
2025-03-13 17:12:07 -0400

    Got the next word button and previous word button working. Note: commit after each thing accomplished (ie commit after getting next button working and again after getting prev button working)

--- flashcard.py
+++ flashcard.py
@@ -34,11 +34,9 @@ word_dict = {
 
 
 # State variable
-idx = 1
-test_word = StringVar(value="TESING")
-test_word2 = test_words[idx]
-word = StringVar(value=test_word2)
-# word.trace_add('w', callback)
+idx = 0
+word = StringVar(value=test_words[0])
+word2 = test_words[0]
 
 # Create and grid outer frame for everything to go into
 mainFrame = ttk.Frame(root, padding=(5, 5, 12, 0))
@@ -46,22 +44,37 @@ mainFrame.grid(column=0, row=0, sticky=(N,W,E,S))
 root.grid_columnconfigure(0, weight=1)
 root.grid_rowconfigure(0, weight=1)
 
+
+# Methods to make the flashcard app work
+def next_word(event=None):
+    global idx
+    idx = (idx + 1) % len(test_words)
+    word.set(test_words[idx])
+
+def prev_word(event=None):
+    global idx
+    if(idx - 1 >= 0):
+        idx = idx - 1
+    else:
+        idx = len(test_words) - 1
+    word.set(test_words[idx])
+
+def callback(name, index, mode):
+    return word.get()
+
 # Create widgets for the page
-flashcard = ttk.Label(mainFrame, text=test_word2)
+flashcard = ttk.Label(mainFrame, textvariable=word)
 next_button = ttk.Button(mainFrame, text="Next Word", padding=(20, 10))
+next_button.bind('<ButtonPress-1>', next_word)
+prev_button = ttk.Button(mainFrame, text="Previous Word", padding=(20, 10))
+prev_button.bind('<ButtonPress-1>', prev_word)
 
 # Grid all the widgets
 flashcard.grid(column=0, row=0, sticky=(N,W,E,S))
-next_button.grid(column=0, row=1, sticky=(N,W,E,S))
+prev_button.grid(column=0, row=1, sticky=(N,W,E,S))
+next_button.grid(column=1, row=1, sticky=(N,W,E,S))
 
 
-# Methods to make the flashcard app work
-def next_word():
-    idx += 1
-    test_word2 = test_words[idx + 1]
-
-def callback(name, index, mode):
-    return var.get()
 
 
 root.mainloop()
