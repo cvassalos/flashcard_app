4868f54b15658d4020653a8e1ecedfcb65fa7eb9
cvassalos <vassalce@gmail.com>
2025-03-19 16:38:09 -0400

    Created two python files for planning for further execution of the flashcard app

---
+++ main.py
@@ -0,0 +1,5 @@
+# This will  be the main window the flashcard app will open up to and give
+# options as to what the user would like to do.
+# Options include:
+#   Review flashcards -> which stack to review
+#   Add new flashcards
