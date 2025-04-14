162143646d6f7a4a757187be6bb897e73321b423
cvassalos <vassalce@gmail.com>
2025-04-14 17:43:03 -0400

    Added some padding, added a button to submit entries, made it so when app opens the focus is on the front of card entry box. Now working on function to add front and back of card to dictionary, which will eventually be saved to a txt file

--- add_flashcards.py
+++ add_flashcards.py
@@ -20,17 +20,27 @@ center_y = int(screen_height / 2 - window_height / 2)
 # Set the position of the window to the center of the screen
 root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 
+# CARDS TO ADD ARRAY ------------------------------------------
+card_stack = {}
+
+# FUNCTIONS -----------------------------------------------
+def add_card_to_stack():
+    # flashcard_front_entry.get
+    pass
+
+
 # FRAME to put all the widgets into
 add_cards_frame = ttk.Frame(root, borderwidth=1)
 add_cards_frame.grid(column=0, row=0)
 
 # Front of flashcard LABEL
 flashcard_front_label = ttk.Label(add_cards_frame, text="Front of Flashcard")
-flashcard_front_label.grid(column=0, row=0)
+flashcard_front_label.grid(column=0, row=0, padx=25, pady=25)
 
 # Front of the flashcard ENTRY BOX
 flashcard_front_entry = ttk.Entry(add_cards_frame)
 flashcard_front_entry.grid(column=1, row=0)
+flashcard_front_entry.focus()
 
 # Back of flashcard LABEL
 flashcard_back_label = ttk.Label(add_cards_frame, text="Back of Flashcard")
@@ -39,5 +49,10 @@ flashcard_back_label.grid(column=0, row=1)
 # Back of the flashcard ENTRY BOX
 flashcard_back = ttk.Entry(add_cards_frame)
 flashcard_back.grid(column=1, row=1)
+
+# Add a button to add the card to the stack
+submit_btn = ttk.Button(add_cards_frame, text="Add Card", command=add_card_to_stack)
+submit_btn.grid(column=0, row=2, columnspan=2, padx=25, pady=25)
+
 
 root.mainloop()
