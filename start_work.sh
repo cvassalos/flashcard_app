14337a421ef064c0e78e9224ab420db3ae92cd2b
Constantinos <vassalce@gmail.com>
2026-05-13 10:45:40 -0400

    Working on script to start workspace

--- start_work.sh
+++ start_work.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-tmux new-session -n "lazygit" "lazygit"
+SESSION="flashcard"
+
+tmux new-session -d -s "$SESSION" -n "lazygit" "lazygit"
+tmux new-window -t "$SESSION:1" -n "php_server" "php -S localhost:8000"
+tmux new-window -t "$SESSION:2" -n "index.php" "nvim index.php"
+tmux new-window -t "$SESSION:3" -n "stack.php" "nvim stack.php"
+
+tmux attach-session -t "$SESSION"
